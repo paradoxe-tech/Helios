d821dca89ebb78fd581487ef6bfb9181c2e0beee
Mathéo Tripnaux-Moreau <89200698+paradoxe-tech@users.noreply.github.com>
2024-10-15 08:30:01 +0000

    [structure] moved home tags declaration to shared/defaults

--- shared/defaults.ts
+++ shared/defaults.ts
@@ -1,24 +1,34 @@
 import * as types from "../../../shared/types";
 
-export const defaultScoreParams:types.ScoreParams = {
+export const defaultScoreParams: types.ScoreParams = {
   lU: 1,
   lA: 1,
   lG: 1,
   allow_sensitive: false,
   strict_child_mode: false,
-  content_language: []
-}
+  content_language: [],
+};
 
-export const devUser:types.User = {
+export const devUser: types.User = {
   username: "dev",
   history: [],
   following: [],
   followers: [],
-}
+};
 
-export const guestUser:types.User = {
+export const guestUser: types.User = {
   username: "guest",
   history: [],
   following: [],
   followers: [],
-}
+};
+
+export const tags = [
+  "Tout",
+  "Science",
+  "Divertissement",
+  "Actualités",
+  "Sports",
+  "Lifestyle",
+  "Gaming",
+];
