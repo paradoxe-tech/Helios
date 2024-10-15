956b1adf2b8581b5742d731845cf450f36d0b395
Mathéo Tripnaux-Moreau <89200698+paradoxe-tech@users.noreply.github.com>
2024-10-15 07:18:41 +0000

    config now written in typescript (caused #5)

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,9 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    // Proxy all API requests to the Express server
     proxy: {
-      '/api': 'http://localhost:3000', // Proxy to Express API
+      '/api': 'http://localhost:3000'
     },
-  },
+  }
 });
