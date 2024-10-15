73e59f961ca6eee6e61c77873d16ce764e91fa1e
Mathéo Tripnaux-Moreau <89200698+paradoxe-tech@users.noreply.github.com>
2024-10-15 22:30:14 +0000

    [server] applied parallelization to the recommender async call

--- server/src/algo/recommend.ts
+++ server/src/algo/recommend.ts
@@ -1,46 +1,41 @@
 import Video from "./Video";
-import { User, VideoData, ScoreParams } from "../../../shared/types";
+import * as types from "../../../shared/types";
 
-/**
+ /**
  * returns a list of videos associated with detailed scores, sorted by
  * overall score and limited to `n` elements.
  * @param {number} n : number of elements to be returned
  * @param {ScoreParams} params : dictionnary of the recommender parameters
  * @returns {Array<VideoData>} : a sorted list of scored video
  */
-async function recommend(
-  videosIds: Array<string>,
-  n: number,
-  user: User,
-  params: ScoreParams,
-): Promise<Array<VideoData>> {
-  let videos = [];
+async function recommend(vidIds: string[], n: number, user: types.User, params: types.ScoreParams): Promise<types.VideoData[]> {
+  const videos = await Promise.all(
+    vidIds.map(async (id) => {
+      try {
+        // instanciate video (and request youtube API)
+        const video = await Video.create(id, process.env["YOUTUBE_API_KEY"]);
 
-  for (let id of videosIds) {
-    let video = await Video.create(id, process.env["YOUTUBE_API_KEY"]);
+        // filter according to provided parameters
+        if (params.strict_child_mode && !video.childish) return null;
+        if (!params.allow_sensitive && video.sensitive) return null;
+        if (params.content_language.length !== 0) {
+          if (!params.content_language.includes(video.language)) return null;
+        } 
 
-    // pre-filter the dataset to avoid useless compute units
-    if (params.content_language.length == 0) {}
-    else if (!params.content_language.includes(video.language)) continue;
-    if (!params.allow_sensitive && video.sensitive) continue;
-    if (params.strict_child_mode && !video.childish) continue;
+        // compute scores and return the results
+        const scores = await video.score(user, params);
+        return { video, scores };
+      } catch (error) {
+        return null;
+      }
+    }),
+  );
 
-    videos.push({
-      video: video,
-      scores: await video.score(user, params),
-    });
-  }
-
-  let results = videos.sort((a: VideoData, b: VideoData) => {
-    let scoreA: number = a.scores.score;
-    let scoreB: number = b.scores.score;
-
-    if (scoreA > scoreB) return -1;
-    if (scoreA < scoreB) return 1;
-    return 0;
-  });
-
-  return results.slice(0, n);
+  // keep only the existant ones and sort by score
+  return videos
+    .filter(v => v !== null)
+    .sort((a, b) => b.scores.score - a.scores.score)
+    .slice(0, n);
 }
 
 export default recommend;
