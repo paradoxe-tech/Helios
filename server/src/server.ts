5e30a21f4d29ea80b89e78596bf6db8b6bbd1ed4
Mathéo Tripnaux-Moreau <89200698+paradoxe-tech@users.noreply.github.com>
2024-10-19 10:53:47 +0000

    [server] connected MongoDB cluster and users collection

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,21 +2,39 @@ import express from "express";
 import { fileURLToPath } from "url";
 import path, { dirname } from "path";
 import fs from "fs";
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const ROOT = path.resolve(__dirname, "../../");
-
 const app = express();
 
 // express middlewares for url and json format
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const mongoClient = new MongoClient(process.env['MDB_CONNECTION_STRING'], {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+await mongoClient.connect();
+let database = mongoClient.db("helios-users");
+
+// express middleware to check for DB availability
+app.use((req, res, next) => {
+  if (!database) {
+    return res.status(500).send("Database not connected yet.");
+  } else next();
+})
+
 const apiPath = path.join(ROOT, '/server/src/api');
 for(let endpoint of fs.readdirSync(apiPath)) {
   let imported = await import(path.join(apiPath, endpoint));
-  imported.default(app, ROOT);
+  imported.default(app, ROOT, database);
 }
 
 // serve static files from the React app
